fc773c88cca48d5ab8aec74a159fec2f1c4d3d48
Felipe Signoretti <cesar.junior@certsys.com.br>
2024-12-09 22:23:11 -0300

    criando menu e add método listar séries buscadas

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,9 @@
 package br.com.alura.screenmatch;
 
-
 import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -13,22 +11,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
-
 	@Override
 	public  void run(String[] args) {
 		Principal principal = new Principal();
 		principal.exibeMenu();
-
-
-
-
-
-
-
-
-
-
-
 	}
 
 }
--- src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverteDados implements IConverteDados {
     private ObjectMapper mapper = new ObjectMapper();
-
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
         T result;
