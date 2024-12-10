f0b5d16776feef15976d31ae967ce9a0f391c956
Felipe Signoretti <cesar.junior@certsys.com.br>
2024-12-09 21:42:38 -0300

    organizando arquivos e add avaliação média de episódio usando sumary estatistics

--- src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
@@ -1,16 +1,11 @@
-package br.com.alura.screenmatch.service;
+package br.com.alura.screenmatch.model;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosTemporada(@JsonAlias("Season") Integer numero,
-                             @JsonAlias("Episodes") List<DadosEpisodio> episodios){
-
-
-
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
 }
-
-
